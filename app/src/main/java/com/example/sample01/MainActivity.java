95ab776b65c7d593707473050baf08dd5488493e
yonini <hardworkingini@gmail.com>
2022-11-13 16:25:52 +0900

    카카오 map api 연동 오류 수정

--- app/src/main/java/com/example/sample01/MainActivity.java
+++ app/src/main/java/com/example/sample01/MainActivity.java
@@ -17,12 +17,6 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.example.sample01.DataBase.ChoiceDataBaseHelper;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.view.ViewGroup;
 import net.daum.mf.map.api.MapView;
